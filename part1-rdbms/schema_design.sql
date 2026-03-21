c53e05e1738c1cf5bdcfde8b3cea04b62ce9a0f4
Shivangi Kanojia <kanojiashivi23@gmail.com>
2026-03-21 23:31:10 +0530

    Update schema_design.sql

--- part1-rdbms/schema_design.sql
+++ part1-rdbms/schema_design.sql
@@ -37,3 +37,39 @@ CREATE TABLE order_items (
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
+
+INSERT INTO customers VALUES
+(1, 'Amit', 'Mumbai'),
+(2, 'Rahul', 'Delhi'),
+(3, 'Sneha', 'Pune'),
+(4, 'Priya', 'Mumbai'),
+(5, 'Karan', 'Bangalore');
+
+INSERT INTO products VALUES
+(101, 'Laptop', 60000),
+(102, 'Phone', 20000),
+(103, 'Shoes', 3000),
+(104, 'Watch', 5000),
+(105, 'Headphones', 1500);
+
+INSERT INTO sales_rep VALUES
+(1, 'Raj'),
+(2, 'Simran'),
+(3, 'Aman'),
+(4, 'Neha'),
+(5, 'Vikas');
+
+INSERT INTO orders VALUES
+(1001, 1, 1, '2023-01-10'),
+(1002, 2, 2, '2023-02-12'),
+(1003, 3, 3, '2023-03-15'),
+(1004, 4, 4, '2023-04-18'),
+(1005, 5, 5, '2023-05-20');
+
+INSERT INTO order_items VALUES
+(1, 1001, 101, 1),
+(2, 1002, 102, 2),
+(3, 1003, 103, 1),
+(4, 1004, 104, 3),
+(5, 1005, 105, 2);
+
